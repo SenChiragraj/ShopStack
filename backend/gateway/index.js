7a49567723fd7efdcacd036c1e1cde762d1d8e65
SenChiragraj <senChiragraj@gmail.com>
2024-04-30 19:00:57 +0530

    vercel check

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -11,6 +11,10 @@ app.use("/order", proxy("http://localhost:8003")); // orders, carts
 app.use("/product", proxy("http://localhost:8002")); // products
 app.use("/", proxy("http://localhost:8001")); // auth, user info
 
+app.get('/', (req, res) => {
+  return res.status(201).json({message : 'Site is healthy👍'})
+})
+
 app.listen(8000, () => {
   console.log("Gateway is Listening to Port 8000");
 });
